7b5a78008de29062ff7c1197a7bcf3718a9eb0cc
Rohith Raju <rohith.el.raj@gmail.com>
2023-03-30 14:49:14 +0200

    eventPlan added

--- src/main/webapp/app/entities/entity-routing.module.ts
+++ src/main/webapp/app/entities/entity-routing.module.ts
@@ -4,6 +4,11 @@ import { RouterModule } from '@angular/router';
 @NgModule({
   imports: [
     RouterModule.forChild([
+      {
+        path: 'event-plan',
+        data: { pageTitle: 'hikesApp.eventPlan.home.title' },
+        loadChildren: () => import('./event-plan/event-plan.module').then(m => m.EventPlanModule),
+      },
       /* jhipster-needle-add-entity-route - JHipster will add entity modules routes here */
     ]),
   ],
